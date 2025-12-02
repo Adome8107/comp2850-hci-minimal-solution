79015470dff46d58ea8a9d72843c30e33a2e4932
Adam Hassnain [zzvn8107] <zzvn8107@uol-pc-051694.leeds.ac.uk>
2025-12-02 14:05:01 +0000

    trying to add labels

--- src/main/kotlin/model/Task.kt
+++ src/main/kotlin/model/Task.kt
@@ -21,6 +21,7 @@ data class Task(
     val title: String,
     val completed: Boolean = false,
     val createdAt: LocalDateTime = LocalDateTime.now(),
+    val label: String,
 ) {
     companion object {
         /**
@@ -80,7 +81,7 @@ data class Task(
     fun toCSV(): String {
         val formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME
         val escapedTitle = title.replace("\"", "\"\"")
-        return "$id,\"$escapedTitle\",$completed,${createdAt.format(formatter)}"
+        return "$id,\"$escapedTitle\",$completed,${createdAt.format(formatter)}, $label"
     }
 
     /**
@@ -104,6 +105,7 @@ data class Task(
             "completed" to completed,
             "createdAt" to createdAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
             "createdAtISO" to createdAt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+            "label" to label
         )
 }
 
